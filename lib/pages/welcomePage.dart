4ca892bbbfac54a8afac5afa9200939df4a476a5
mutlucanunal <mutlucan.unal@gmail.com>
2023-03-27 23:18:35 +0300

    23:18 27.03.2023

--- lib/pages/welcomePage.dart
+++ lib/pages/welcomePage.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:hive/hive.dart';
@@ -95,125 +96,160 @@ class _WelcomePageState extends State<WelcomePage> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+          resizeToAvoidBottomInset: false,
           body: Container(
-        child: Center(
             child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("WELCOME"),
-            TextField(
-              inputFormatters: [
-                LengthLimitingTextInputFormatter(40),
-                FilteringTextInputFormatter.deny("%"),
-              ],
-              autofocus: true,
-              onChanged: (value2) {
-                setState(() {
-                  _textFieldValue = value2;
-                });
-              },
-              controller: _turkceTextFieldController,
-              decoration: InputDecoration(
-                contentPadding: EdgeInsets.all(10),
-                isCollapsed: true,
-                filled: true,
-                fillColor: Color.fromARGB(255, 216, 255, 161),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30),
-                  borderSide: BorderSide(
-                    color: Colors.green,
-                    width: 1.0,
-                  ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(30),
-                  borderSide: BorderSide(
-                    color: Colors.purple,
-                    width: 2.0,
-                  ),
-                ),
-                hintText: "Username",
-                hintStyle: TextStyle(color: Color.fromARGB(75, 21, 9, 35)),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
-              child: DropdownButton<String>(
-                  dropdownColor: Color(0xff010114).withOpacity(1),
-                  value: _languageFull,
-                  items: _languageDropdown.keys.toList().map((dynamic value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: new Text(value,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  height: MediaQuery.of(context).size.height * 3 / 5,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 50, 0, 20),
+                        child: Text(
+                          "WELCOME",
                           style: TextStyle(
-                              color: Color.fromARGB(255, 137, 111, 111),
-                              fontWeight: FontWeight.bold)),
-                    );
-                  }).toList(),
-                  onChanged: (value) {
-                    setState(() {
-                      _languageFull = value!;
-                    });
+                              fontWeight: FontWeight.w800, fontSize: 40),
+                        ),
+                      ),
+                      Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(25, 0, 25, 0),
+                            child: TextField(
+                              inputFormatters: [
+                                LengthLimitingTextInputFormatter(40),
+                                FilteringTextInputFormatter.deny("%"),
+                              ],
+                              autofocus: true,
+                              onChanged: (value2) {
+                                setState(() {
+                                  _textFieldValue = value2;
+                                });
+                              },
+                              controller: _turkceTextFieldController,
+                              decoration: InputDecoration(
+                                contentPadding: EdgeInsets.all(10),
+                                isCollapsed: true,
+                                filled: true,
+                                fillColor: Color.fromARGB(255, 216, 255, 161),
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(30),
+                                  borderSide: BorderSide(
+                                    color: Colors.green,
+                                    width: 1.0,
+                                  ),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(30),
+                                  borderSide: BorderSide(
+                                    color: Colors.purple,
+                                    width: 2.0,
+                                  ),
+                                ),
+                                hintText: "Username",
+                                hintStyle: TextStyle(
+                                    color: Color.fromARGB(75, 21, 9, 35)),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(0, 5, 0, 0),
+                            child: DropdownButton<String>(
+                                borderRadius: BorderRadius.circular(10),
+                                alignment: AlignmentDirectional.topCenter,
+                                dropdownColor: Color(0xff010114).withOpacity(1),
+                                value: _languageFull,
+                                items: _languageDropdown.keys
+                                    .toList()
+                                    .map((dynamic value) {
+                                  return DropdownMenuItem<String>(
+                                    value: value,
+                                    child: new Text(value,
+                                        style: TextStyle(
+                                            color: Color.fromARGB(
+                                                255, 137, 111, 111),
+                                            fontWeight: FontWeight.bold)),
+                                  );
+                                }).toList(),
+                                onChanged: (value) {
+                                  setState(() {
+                                    _languageFull = value!;
+                                  });
 
-                    if (_configData['supportedLanguages'] != null) {
-                      setState(() {
-                        _currentLanguage =
-                            _configData['supportedLanguages'][_languageFull];
-                        box.put("languageSelectedBefore", _currentLanguage);
-                      });
-                    }
+                                  if (_configData['supportedLanguages'] !=
+                                      null) {
+                                    setState(() {
+                                      _currentLanguage =
+                                          _configData['supportedLanguages']
+                                              [_languageFull];
+                                      box.put("languageSelectedBefore",
+                                          _currentLanguage);
+                                    });
+                                  }
 
-                    // if (_languageFull == "Español") {
-                    //   setState(() {
-                    //     _currentLanguage = "es";
-                    //   });
-                    // } else if (value == "Deutsch") {
-                    //   setState(() {
-                    //     _currentLanguage = "de";
-                    //   });
-                    // } else if (value == "Français") {
-                    //   setState(() {
-                    //     _currentLanguage = "fr";
-                    //   });
-                    // } else if (value == "English") {
-                    //   setState(() {
-                    //     _currentLanguage = "en";
-                    //   });
-                    // } else {
-                    //   setState(() {
-                    //     _currentLanguage = "tr";
-                    //   });
-                    // }
+                                  // if (_languageFull == "Español") {
+                                  //   setState(() {
+                                  //     _currentLanguage = "es";
+                                  //   });
+                                  // } else if (value == "Deutsch") {
+                                  //   setState(() {
+                                  //     _currentLanguage = "de";
+                                  //   });
+                                  // } else if (value == "Français") {
+                                  //   setState(() {
+                                  //     _currentLanguage = "fr";
+                                  //   });
+                                  // } else if (value == "English") {
+                                  //   setState(() {
+                                  //     _currentLanguage = "en";
+                                  //   });
+                                  // } else {
+                                  //   setState(() {
+                                  //     _currentLanguage = "tr";
+                                  //   });
+                                  // }
 
-                    print(_currentLanguage);
-                  }),
+                                  print(_currentLanguage);
+                                }),
+                          ),
+                        ],
+                      ),
+                      RawMaterialButton(
+                          fillColor: _textFieldValue.toString().length == 0
+                              ? Color.fromARGB(255, 74, 74, 79).withOpacity(0.3)
+                              : Color.fromARGB(255, 33, 39, 120),
+                          shape: RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(15.0))),
+                          splashColor: Color(0xff77A830),
+                          textStyle: TextStyle(color: Colors.white),
+                          child: Text("Next",
+                              style: TextStyle(
+                                color: _textFieldValue.toString().length == 0
+                                    ? Color.fromARGB(255, 255, 255, 255)
+                                    : Colors.amber,
+                                fontSize: 15,
+                                fontFamily: 'Times New Roman',
+                                // fontWeight: FontWeight.bold
+                              )),
+                          onPressed: _textFieldValue.toString().length == 0
+                              ? null
+                              : () async {
+                                  var a = await _authService.anonymSignIn(
+                                      _turkceTextFieldController.text);
+                                }),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height / 3,
+                )
+              ],
             ),
-            RawMaterialButton(
-                fillColor: _textFieldValue.toString().length == 0
-                    ? Color.fromARGB(255, 74, 74, 79).withOpacity(0.3)
-                    : Color.fromARGB(255, 33, 39, 120),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(15.0))),
-                splashColor: Color(0xff77A830),
-                textStyle: TextStyle(color: Colors.white),
-                child: Text("Next",
-                    style: TextStyle(
-                      color: _textFieldValue.toString().length == 0
-                          ? Color.fromARGB(255, 255, 255, 255)
-                          : Colors.amber,
-                      fontSize: 15,
-                      fontFamily: 'Times New Roman',
-                      // fontWeight: FontWeight.bold
-                    )),
-                onPressed: _textFieldValue.toString().length == 0
-                    ? null
-                    : () async {
-                        var a = await _authService
-                            .anonymSignIn(_turkceTextFieldController.text);
-                      }),
-          ],
-        )),
-      )),
+          )),
     );
   }
 }
